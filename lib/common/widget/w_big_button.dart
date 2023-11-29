f9c594d2d40474dce52c93afeea93ebcf06097a4
khanian <khanylee@gmail.com>
2023-11-29 17:20:24 +0900

    make appbar and bottom scroll

--- lib/common/widget/w_big_button.dart
+++ lib/common/widget/w_big_button.dart
@@ -1,5 +1,4 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/common/dart/extension/context_extension.dart';
 import 'package:fast_app_base/common/widget/w_arrow.dart';
 import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:flutter/material.dart';
@@ -12,13 +11,16 @@ class BigButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RoundedContainer(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          text.text.white.size(20).bold.make(),
-          const Arrow(),
-        ],
+    return Tap(
+      onTap: onTap,
+      child: RoundedContainer(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            text.text.white.size(20).bold.make(),
+            const Arrow(),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -3,6 +3,11 @@ import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_account.dart';
 
 final bankAccountShinhan1 =
     BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+final bankAccountShinhan4 = BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+final bankAccountShinhan5 = BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+final bankAccountShinhan6 = BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+final bankAccountShinhan7 = BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+final bankAccountShinhan8 = BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
 final bankAccountShinhan2 =
     BankAccount(bankShinhan, 30000000, accountTypeName: "저축예금");
 final bankAccountShinhan3 =
@@ -37,6 +42,11 @@ final bankAccounts = [
   bankAccountShinhan1,
   bankAccountShinhan2,
   bankAccountShinhan3,
+  bankAccountShinhan4,
+  bankAccountShinhan5,
+  bankAccountShinhan6,
+  bankAccountShinhan7,
+  bankAccountShinhan8,
   bankAccountToss,
   bankAccountKakao
 ];
